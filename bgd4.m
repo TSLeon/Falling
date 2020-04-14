cd37c7926d092ecfa241976f3cf9f62e04c4c5bc
william <aimerchenyuan@163.com>
2020-04-14 18:44:33 +0800

    change double class to int16 class ;

--- bgd4.m
+++ bgd4.m
@@ -2,24 +2,23 @@
 	% first prepar
 	imgroot = 'source\test3\';
 	M = 60;
-	figure;
-	
+	figure;	
 	% get size of image,and background
 	[x,y,z] = size(imread([imgroot,num2str(1),'.jpg']));
-	u_xy = zeros(x,y,z);
+	u_xy = int16(zeros(x,y,z));
 	u_xy = rgb2gray(u_xy);
 	for t=1:M
 		u_temp = imread([imgroot,num2str(t),'.jpg']);
 		u_temp = rgb2gray(u_temp);
-		u_xy = u_xy + double(u_temp);
+		u_xy = u_xy + int16(u_temp);
 	end
 	% check data
 	%u_temp = u_xy;
 	u_xy = u_xy/M;
-	imshow(uint8(u_xy));	
-	
+%	imshow(uint8(u_xy));
 	% diff between frame
-	F_t = zeros(x,y,z);
+	F_t = int16(zeros(x,y,z));
+	F_t = rgb2gray(F_t);
 	inter = 3;
 	for i=inter+1:M
 		I1 = imread([imgroot,num2str(t),'.jpg']);
@@ -27,13 +26,12 @@
 		I1 = rgb2gray(I1);
 		I2 = rgb2gray(I2);
 		f_t = abs(I1-I2);
-		F_t = F_t + double(f_t);
+		F_t = F_t + int16(f_t);
 	end
 	u_diff = F_t/M;
-
 	% standard diff
-	Diff = zeros(x,y,z);
-	diff = zeros(x,y,z);
+	Diff = int16(zeros(x,y,z));
+	diff = int16(zeros(x,y,z));
 	Diff = rgb2gray(Diff);
 	diff = rgb2gray(diff);
 	for t=inter+1:M
@@ -42,27 +40,24 @@
 		I1 = rgb2gray(I1);
 		I2 = rgb2gray(I2);
 		f_t = abs(I1-I2);
-		f_t = double(f_t);
+		f_t = int16(f_t);
 		diff = (f_t-u_diff).*(f_t-u_diff);
 		Diff = Diff + diff;
 	end
 	diff_std_insqrt = Diff/M;
-	diff_std = sqrt(diff_std_insqrt);
-	
+	diff_std = sqrt(double(diff_std_insqrt));	
 	% compute threshold
-	k = 1;
+	%k = 1;
 	peta = 2;
-	threshold = u_diff + peta*diff_std;
-	
+	for k=1:60
+	threshold = u_diff + peta*int16(diff_std);	
 	% get present image
 	I = imread([imgroot,num2str(M+k),'.jpg']);	
 	I = rgb2gray(I);
-
 	% make diff to get moving target
-	d_xy = abs(u_xy - double(I));
-	
+	d_xy = abs(u_xy - int16(I));  %--------------------------------------------------------outplay	
 	% get outplay
-	out_target = zeros(x,y,z);
+	out_target = int16(zeros(x,y,z));
 	out_target = rgb2gray(out_target);
 	for i=1:x
 		for j=1:y
@@ -73,11 +68,10 @@
 			end
 		end
 	end
-
-	% update
-	alpha = 0.65; % between 0-1
+	% ---------------------------------------------------------------------------------------update
+	alpha = 0.2; % between 0-1
 	% update background
-	u_xy = alpha*double(I) + (1-alpha)*u_xy;
+	u_xy = alpha*int16(I) + (1-alpha)*u_xy;
 	for i=1:x
 		for j=1:y
 			if u_xy(i,j) > 255
@@ -85,25 +79,10 @@
 			end
 		end
 	end
-
-	d_xy_updated = abs(u_xy - double(I));
-	out_target_updated = zeros(x,y,z);
-	out_target_updated = rgb2gray(out_target_updated);
-	for i=1:x
-		for j=1:y
-			if d_xy_updated(i,j) > threshold(i,j)
-				out_target_updated(i,j) = 255;
-			else
-				out_target_updated(i,j) = 0;
-			end
-		end
-	end
-
 	% update frame diff
 
 	% update standard diff
-
-	%------------------------------------
+	%------------------------------------------------------------------------------------------show
 	%subplot(3,3,1);
 	imshow(uint8(u_xy));
 	title('average background');
@@ -130,16 +109,9 @@
 
 	imshow(uint8(out_target));
 	title('out target');
-	saveas(gcf,['test\','out_target.jpg']);
+	saveas(gcf,['test\out_target',num2str(k),'.jpg']);
 
-	imshow(uint8(d_xy_updated));
-	title('d_xy updated');
-	saveas(gcf,['test\','d_xy_updated.jpg']);
-
-	imshow(uint8(out_target_updated));
-	title('out_target updated');
-	saveas(gcf,['test\','out_target_updated.jpg']);
-
+	end
 	img_BW = u_xy;
 %end
 
