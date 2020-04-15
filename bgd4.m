1fe87bdb861ff00dcefbd36e079d56587784cf32
william <aimerchenyuan@163.com>
2020-04-15 21:37:09 +0800

    test

--- bgd4.m
+++ bgd4.m
@@ -1,26 +1,27 @@
 %function img_BW = bgd4()
 	% first prepar
-	imgroot = 'source\test3\';
+	imgroot = 'source\vtest.avi\';
 	M = 60;
+	start = 1;
 	figure;	
 	% get size of image,and background
 	[x,y,z] = size(imread([imgroot,num2str(1),'.jpg']));
 	u_xy = int16(zeros(x,y,z));
 	u_xy = rgb2gray(u_xy);
-	for t=1:M
+	for t=start:M
 		u_temp = imread([imgroot,num2str(t),'.jpg']);
 		u_temp = rgb2gray(u_temp);
 		u_xy = u_xy + int16(u_temp);
 	end
 	% check data
 	%u_temp = u_xy;
-	u_xy = u_xy/M;
+	u_xy = u_xy/(M-start+1);
 %	imshow(uint8(u_xy));
 	% diff between frame
 	F_t = int16(zeros(x,y,z));
 	F_t = rgb2gray(F_t);
 	inter = 3;
-	for i=inter+1:M
+	for i=inter+start:M
 		I1 = imread([imgroot,num2str(t),'.jpg']);
 		I2 = imread([imgroot,num2str(t-inter),'.jpg']);
 		I1 = rgb2gray(I1);
@@ -28,13 +29,13 @@
 		f_t = abs(I1-I2);
 		F_t = F_t + int16(f_t);
 	end
-	u_diff = F_t/M;
+	u_diff = F_t/(M-start+1);
 	% standard diff
 	Diff = int16(zeros(x,y,z));
 	diff = int16(zeros(x,y,z));
 	Diff = rgb2gray(Diff);
 	diff = rgb2gray(diff);
-	for t=inter+1:M
+	for t=inter+start:M
 		I1 = imread([imgroot,num2str(t),'.jpg']);
 		I2 = imread([imgroot,num2str(t-inter),'.jpg']);
 		I1 = rgb2gray(I1);
@@ -44,7 +45,7 @@
 		diff = (f_t-u_diff).*(f_t-u_diff);
 		Diff = Diff + diff;
 	end
-	diff_std_insqrt = Diff/M;
+	diff_std_insqrt = Diff/(M-start+1);
 	diff_std = sqrt(double(diff_std_insqrt));	
 	% compute threshold
 	%k = 1;
@@ -80,36 +81,62 @@
 		end
 	end
 	% update frame diff
-
+	I_1 = rgb2gray(imread([imgroot,num2str(M+k-inter),'.jpg']));
+	f_x = abs(int16(I)-int16(I_1));
+	u_diff = (1-alpha)*u_diff + alpha*f_x;
 	% update standard diff
+	diff_std = (1-alpha)*int16(diff_std) + alpha*abs(f_x - u_diff);
+	%---------------------------------------------------------------------------------------process
+	% CLAHE
+	L = adapthisteq(uint8(out_target),'NumTiles',[8,8],'ClipLimit',0.005);
+	% gaussian
+	H = fspecial('gaussian',3,1);
+	filteredBW = imfilter(L,H);
+	uSobel = filteredBW;
+    [X,Y] = size(filteredBW);
+    F = double(filteredBW);
+    for x=2:X-1
+        for y=2:Y-1
+            Gx = (F(x+1,y-1)+2*F(x+1,y)+F(x+1,y+1)) - (F(x-1,y-1)+2*F(x-1,y)+F(x-1,y+1));
+            Gy = (F(x-1,y-1)+2*F(x,y-1)+F(x+1,y-1))-(F(x-1,y+1)+2*F(x,y+1)+F(x+1,y+1));
+            uSobel(x,y) = sqrt(Gx^2+Gy^2);
+        end
+    end
+	%-----------------------------------------------------------------------------------------show2
+	imshow(L);
+	saveas(gcf,['test\L',num2str(k),'.jpg']);
+	imshow(filteredBW);
+	saveas(gcf,['test\filteredBW',num2str(k),'.jpg']);
+	imshow(uSobel);
+	saveas(gcf,['test\uSobel',num2str(k),'.jpg']);
 	%------------------------------------------------------------------------------------------show
 	%subplot(3,3,1);
-	imshow(uint8(u_xy));
-	title('average background');
-	saveas(gcf,['test\','average_background.jpg']);
+	%imshow(uint8(u_xy));
+	%title('average background');
+	%saveas(gcf,['test\average_backround',num2str(k),'.jpg']);
 
 	%subplot(3,3,2);
-	imshow(uint8(u_diff));
-	title('frame diff');
-	saveas(gcf,['test\','frame_diff.jpg']);
+	%imshow(uint8(u_diff));
+	%title('frame diff');
+	%saveas(gcf,['test\u_diff',num2str(k),'.jpg']);
 
 	%subplot(3,3,3);
-	imshow(uint8(diff_std));
-	title('standard diff');
-	saveas(gcf,['test\','standard_diff.jpg']);
+	%imshow(uint8(diff_std));
+	%title('standard diff');
+	%saveas(gcf,['test\diff_std',num2str(k),'.jpg']);
 
 	%subplot(3,3,4);
-	imshow(uint8(threshold));
-	title('threshold');
-	saveas(gcf,['test\','threshold.jpg']);
+	%imshow(uint8(threshold));
+	%title('threshold');
+	%saveas(gcf,['test\threshold',num2str(k),'.jpg']);
 
-	imshow(uint8(d_xy));
-	title('d_xy');
-	saveas(gcf,['test\','d_xy.jpg']);
+	%imshow(uint8(d_xy));
+	%title('d_xy');
+	%saveas(gcf,['test\d_xy',num2str(k),'.jpg']);
 
-	imshow(uint8(out_target));
-	title('out target');
-	saveas(gcf,['test\out_target',num2str(k),'.jpg']);
+	%imshow(uint8(out_target));
+	%title('out target');
+	%saveas(gcf,['test\out_target',num2str(k),'.jpg']);
 
 	end
 	img_BW = u_xy;
--- videoProcess.m
+++ videoProcess.m
@@ -1,5 +1,5 @@
-videoFile = 'vtest.avi';
-file_path = 'source\vtest.avi';
+videoFile = 'test5.mp4';
+file_path = 'source\test5';
 if exist(file_path)==0
     mkdir(file_path);
 end
@@ -7,7 +7,7 @@ videObj = VideoReader(videoFile);
 totalFrames = videObj.NumberOfFrames;
 for frame = 1:totalFrames
 	frameImg = read(videObj,frame);
-	saveName = ['source\vtest.avi\',num2str(frame),'.jpg'];
+	saveName = ['source\test5\',num2str(frame),'.jpg'];
 	imwrite(frameImg,saveName,'jpg');
 end
 
